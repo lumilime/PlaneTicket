482b6981dd7438c221d7c5ac08e265def7df5fad
lumilime <127956335+lumilime@users.noreply.github.com>
2023-04-06 14:38:53 +0700

    Add files via upload

---
+++ PlaneTicket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass2
+{
+    class PlaneTicket
+    {
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string currency { get; set; }
+        public int cost { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("oringin : {0}", origin);
+            Console.WriteLine("destination :{0}", destination);
+            Console.WriteLine("currency :{0}", currency);
+            Console.WriteLine("cost :{0} ({1})", cost, currency);
+            if (currency == "IDR")
+            {
+                cost = cost / 15000;
+                Console.WriteLine("convert to Dollar: {0}", cost);
+
+            }else if (currency == "USD")
+            {
+                cost = cost * 15000;
+                Console.WriteLine("Convert to Rupiah: {0}", cost);
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket satu = new PlaneTicket();
+            PlaneTicket dua = new PlaneTicket();
+
+            satu.origin = "JOGJA";
+            satu.destination = "PKU";
+            satu.cost = 100;
+            satu.currency = "USD";
+            satu.PrintSummary();
+
+            Console.WriteLine();
+
+            dua.origin = "JKT";
+            dua.destination = "PKU";
+            dua.cost = 750000;
+            dua.currency = "IDR";
+            dua.PrintSummary();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
